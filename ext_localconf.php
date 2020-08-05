b8bb99d6a0dd90b486222455a20d9b7644b8a7ac
Christian Hackl <hackl.chris@googlemail.com>
2020-08-05 12:05:45 +0200

    replace custom css js fluid include with native TYPO3 core ones

--- ext_localconf.php
+++ ext_localconf.php
@@ -21,18 +21,6 @@ call_user_func(function() {
         // BackendLayouts
         'hh_one_column' => 'BackendIcons/header-1col.png',
         'hh_navaside_left' => 'BackendIcons/header-sidenav-left.png',
-
-        // Gridelements
-        'hh_grid_1_column' => 'gridelements/1_column.png',
-        'hh_grid_2_column' => 'gridelements/2_column.png',
-        'hh_grid_3_column' => 'gridelements/3_column.png',
-        'hh_grid_4_column' => 'gridelements/4_column.png',
-        'hh_grid_25-75_column' => 'gridelements/25-75.png',
-        'hh_grid_33-66_column' => 'gridelements/33-66.png',
-        'hh_grid_66-33_column' => 'gridelements/66-33.png',
-        'hh_grid_75-25_column' => 'gridelements/75-25.png',
-        'hh_grid_slider' => 'gridelements/slider.png',
-        'hh_grid_tabs' => 'gridelements/tabs.png'
     ];
 
     $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
@@ -78,10 +66,6 @@ call_user_func(function() {
         );
 
     // Register Hooks
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['usePageCache'][] =
-        'HauerHeinrich\\HhThemeDefault\\Hooks\\AssetsHook';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] =
-        'HauerHeinrich\\HhThemeDefault\\Hooks\\AssetsHook->usePageCache';
 
     // after Install - Add AdditionalConfiguration.php if not exist
     /** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $signalSlotDispatcher */
@@ -89,7 +73,7 @@ call_user_func(function() {
     $signalSlotDispatcher->connect(
         \TYPO3\CMS\Extensionmanager\Utility\InstallUtility::class,
         'afterExtensionInstall',
-        HauerHeinrich\HhThemeDefault\Signals\AfterExtensionInstall::class,
+        \HauerHeinrich\HhThemeDefault\EventListener\AfterExtensionInstall::class,
         'addAdditionalConfiguration'
     );
 });
