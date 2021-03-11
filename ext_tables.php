8fd405dce74f5091a96f6ca5739634fbd755516e
Christian Hackl <hackl.chris@googlemail.com>
2021-03-11 13:08:15 +0100

    [FEATURE] hide sites-module at production

--- ext_tables.php
+++ ext_tables.php
@@ -19,5 +19,24 @@ call_user_func(function() {
             = \HauerHeinrich\HhThemeDefault\Hooks\BackendControllerHook::class . '->addJavaScript';
     }
 
+    // Backend CSS
     $GLOBALS['TBE_STYLES']['skins']['backend']['stylesheetDirectories']['theme'] = 'EXT:'.$extensionKey.'/Resources/Public/Css/Backend/';
+
+    // Backend modules to hide
+    // example "sites" - module
+    $typo3Context = \TYPO3\CMS\Core\Core\Environment::getContext()->__toString();
+
+    if($typo3Context === 'Production' || $typo3Context === 'Development/Server') {
+        $backendModules = [
+            'site' => [
+                'configuration',
+            ],
+        ];
+
+        foreach ($backendModules as $section => $modules) {
+            $modulesToHide = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $GLOBALS['TBE_MODULES'][$section]);
+            $modulesToHide = array_diff($modulesToHide, $modules);
+            $GLOBALS['TBE_MODULES'][$section] = implode(',', $modulesToHide);
+        }
+    }
 });
