6886681c5b5ec1c2652d3c73deaeadb2c269cad2
Teisi <Hackl.chris@googlemail.com>
2023-01-29 16:46:30 +0100

    [TASK] changed AdditionalConfig - better / more beautiful layout

--- System/additional.php
+++ System/additional.php
@@ -3,132 +3,23 @@ if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
-// Production / Live:
-$databaseCredentialsFile = \TYPO3\CMS\Core\Core\Environment::getPublicPath() . '/../typo3_config/typo3_domain.php';
-if (file_exists($databaseCredentialsFile)) { require_once ($databaseCredentialsFile); }
+use \TYPO3\CMS\Core\Core\Environment;
 
-// Default custom settings for all TYPO3 context's
-$customChanges = [
-    'BE' => [
-        'lockSSL' => 1,
-        'loginSecurityLevel' => 'normal',
-        'compressionLevel' => '0',
-        'versionNumberInFilename' => 0,
-        'RTE_imageStorageDir' => 'fileadmin/uploads_rte/',
-        'lockIP' => 4, // DSGVO / GDPR,
-        // only if server has no argon2i
-        'passwordHashing' => [
-            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\BcryptPasswordHash',
-            'options' => [],
-        ]
-    ],
-    'FE' => [
-        'loginSecurityLevel' => 'normal',
-        'compressionLevel' => '0',
-        'noPHPscriptInclude' => '1',
-        'disableNoCacheParameter' => 0,
-        'hidePagesIfNotTranslatedByDefault' => 1,
-        // only if server has no argon2i
-        'passwordHashing' => [
-            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\BcryptPasswordHash',
-            'options' => [],
-        ]
-    ],
-    'HTTP' => [
-        'verify' => 1,
-    ],
-    'SYS' => [
-        'displayErrors' => 0,
-        'errorHandlerErrors' => 20480,
-        'cookieSecure' => 1,
-        'UTF8filesystem' => 1,
-        'clearCacheSystem' => 1,
-        'enableDeprecationLog' => 0,
-        'phpTimeZone' => 'Europe/Berlin',
-        'systemLocale' => 'de_DE.UTF-8',
-        'ipAnonymization' => '2',
-    ],
-];
-$GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive($GLOBALS['TYPO3_CONF_VARS'], (array)$customChanges);
-
-// Production only
-if(\TYPO3\CMS\Core\Core\Environment::getContext()->__toString() === 'Production') {
-    $customProductionChanges = [
-        'LOG' => [
-            'writerConfiguration' => [
-                \TYPO3\CMS\Core\Log\LogLevel::NOTICE => [
-                    \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
-                        'disabled' => true,
-                    ],
-                ],
-                \TYPO3\CMS\Core\Log\LogLevel::WARNING => [
-                    \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
-                        'disabled' => true,
-                    ],
-                ],
-
-                \TYPO3\CMS\Core\Log\LogLevel::ERROR => [
-                    \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
-                        'disabled' => false,
-                        'logFile' => \TYPO3\CMS\Core\Core\Environment::getVarPath() . '/log/typo3_errors.log'
-                    ],
-                ]
-            ],
-        ]
-    ];
-    $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive($GLOBALS['TYPO3_CONF_VARS'], (array)$customProductionChanges);
-}
+// Production / Live - default settings:
+// these settings will be overridden by preview and / or development settings, see below
+$additionalConfig = Environment::getPublicPath() . '/typo3conf/ext/hh_theme_default/System/additional/production.php';
+if (file_exists($additionalConfig)) { require_once ($additionalConfig); }
 
 // Developement - Stage / Preview:
-if(\TYPO3\CMS\Core\Core\Environment::getContext()->__toString() === 'Development/Server') {
-    $databaseCredentialsFile = \TYPO3\CMS\Core\Core\Environment::getPublicPath() . '/../typo3_config/typo3_domain_preview.php';
-    if (file_exists($databaseCredentialsFile)) { require_once ($databaseCredentialsFile); }
+if(Environment::getContext()->__toString() === 'Development/Server') {
+    $additionalConfigPreview = Environment::getPublicPath() . '/typo3conf/ext/hh_theme_default/System/additional/preview.php';
+    if (file_exists($additionalConfigPreview)) { require_once ($additionalConfigPreview); }
 }
 
 // Developement:
-if(\TYPO3\CMS\Core\Core\Environment::getContext()->__toString() === 'Development') {
-    $databaseCredentialsFile = \TYPO3\CMS\Core\Core\Environment::getPublicPath() . '/../typo3_config/typo3_domain_local.php';
-    if (file_exists($databaseCredentialsFile)) { require_once ($databaseCredentialsFile); }
-
-    $customDevelopmentChanges = [
-        'BE' => [
-            'compressionLevel' => '0',
-            'lockSSL' => 0,
-            'versionNumberInFilename' => 0,
-            'debug' => 1
-        ],
-        'FE' => [
-            'compressionLevel' => '0',
-            'debug' => 1,
-            'noPHPscriptInclude' => 1,
-            'disableNoCacheParameter' => 0
-        ],
-        'EXT' => [
-            'extConf' => [
-            ]
-        ],
-        'HTTP' => [
-            'verify' => 0,
-        ],
-        'SYS' => [
-            'displayErrors' => 1,
-            'errorHandlerErrors' => 30466,
-            'cookieSecure' => 0,
-            'sqlDebug' => 1,
-            'enableDeprecationLog' => 'file',
-        ]
-    ];
-
-    $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive($GLOBALS['TYPO3_CONF_VARS'], (array)$customDevelopmentChanges);
-
-    // Disable All Caches (in Development Mode)
-    // disable Caching: https://usetypo3.com/did-you-know.html
-    // (https://medium.com/typo3blog/disabling-typo3-caches-a137667848c9)
-    foreach ($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'] as $cacheName => $cacheConfiguration) {
-        if($cacheName != 'runtime') { // TYPO3 >= 12
-            $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cacheName]['backend'] = \TYPO3\CMS\Core\Cache\Backend\NullBackend::class;
-        }
-    }
+if(Environment::getContext()->__toString() === 'Development') {
+    $additionalConfigDevelopment = Environment::getPublicPath() . '/typo3conf/ext/hh_theme_default/System/additional/development.php';
+    if (file_exists($additionalConfigDevelopment)) { require_once ($additionalConfigDevelopment); }
 }
 
 // Special for windows systems
