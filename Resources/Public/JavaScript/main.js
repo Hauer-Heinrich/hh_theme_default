e4aef0e28fb54ca7d3193adb1be543a6844bb4ed
iocron <info@iocron.com>
2023-07-11 23:51:02 +0200

    [JS] Removed old news-custom.js

--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -1,9 +1,8 @@
 // INFO: The io framework is beeing imported globally (TOOD: Replace io framework)
 // import io from './plugins/io.js'
 // import address from './components/address.js'
-import navmain from './components/nav-main.js'
-import newsCustom from './components-custom/news-custom.js'
 // import spamEmailProtection from './components/email-spam-protection.js'
+import navmain from './components/nav-main.js'
 
 export default {
     // io,
