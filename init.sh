b3aebddedb07415c51ddd2a06ce87cf1ef21862a
iocron <info@iocron.com>
2023-07-11 20:52:56 +0200

    [BUILD_TOOLS] Removed .env and replaced by user prompts

--- init.sh
+++ init.sh
@@ -2,25 +2,59 @@
 
 set -e
 
-if [[ ! -f ".env" ]]; then
-    cp .env.example .env
-    echo "ERROR: Please adjust your .env file first!! Then re-run the command again."
-    exit 1
-fi
+print_in_color() {
+  local color="$1"
+  shift
+  if [[ -z ${NO_COLOR+x} ]]; then
+    printf "$color%b\e[0m\n" "$*";
+  else
+    printf "%b\n" "$*";
+  fi
+}
 
-source ./.env
+red() { print_in_color "\e[31m" "$*"; }
+green() { print_in_color "\e[32m" "$*"; }
+yellow() { print_in_color "\e[33m" "$*"; }
+blue() { print_in_color "\e[34m" "$*"; }
 
+COLOR_RED="\e[31m%b"
+COLOR_GREEN="\e[32m%b"
+COLOR_YELLOW="\e[33m%b"
+COLOR_BLUE="\e[34m%b"
+COLOR_END="\e[0m"
+
+EXTENSION_KEY="$(basename "$PWD")"
 EXTENSION_NAME="$(echo "$EXTENSION_KEY" | sed -E 's/_//g')"
 EXTENSION_NAMESPACE="$(echo "$EXTENSION_KEY" | sed -E 's/(^|\\<|_)([[:alnum:]])/\U\2/g')"
 EXTENSION_NAMESPACE_ES6="$(echo "$EXTENSION_KEY" | sed -E 's/_/-/g')"
 
-echo "Extension file: .env"
+if [[ "$EXTENSION_KEY" == "hh_theme_default" ]]; then
+    printf "%s" "$(yellow "Consider renaming the folder \"$EXTENSION_KEY\" if you want a custom namespace. Are you sure to proceed anyway (y/n)? ")"
+    read CONFIRMATION
+    if [[ ! $CONFIRMATION =~ ^[Yy]$ ]]; then
+        exit 1
+    fi
+fi
+printf "%s" "Enter a fully qualified domain name (e.g. example.com): "
+read EXTENSION_DOMAIN
+
+EXTENSION_DOMAIN_NAME="$(echo "$EXTENSION_DOMAIN" | sed -E 's/(.+)\.(.+)/\1/g')"
+EXTENSION_DOMAIN_TLD="$(echo "$EXTENSION_DOMAIN" | sed -E 's/(.+)\.(.+)/\2/g')"
+
+printf "\n"
 echo "EXTENSION_KEY: $EXTENSION_KEY"
 echo "EXTENSION_NAME: $EXTENSION_NAME"
 echo "EXTENSION_NAMESPACE: $EXTENSION_NAMESPACE"
 echo "EXTENSION_NAMESPACE_ES6: $EXTENSION_NAMESPACE_ES6"
 echo "EXTENSION_DOMAIN_NAME: $EXTENSION_DOMAIN_NAME"
 echo "EXTENSION_DOMAIN_TLD: $EXTENSION_DOMAIN_TLD"
+printf "\n"
+
+printf "%s" "$(yellow "The configuration above will be used to setup your theme. Are you sure to proceed (y/n)? ")"
+read CONFIRMATION
+if [[ ! $CONFIRMATION =~ ^[Yy]$ ]]; then
+    exit 1
+fi
 
 if [[ -n "$EXTENSION_KEY" && -n "$EXTENSION_NAME" && -n "$EXTENSION_NAMESPACE" && -n "$EXTENSION_NAMESPACE_ES6" && -n "$EXTENSION_DOMAIN_NAME" && -n "$EXTENSION_DOMAIN_TLD" ]]; then
     while read file; do
