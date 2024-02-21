fa6ab60896c00967060a986855d2df6ac25bf816
Christian Hackl <hackl.chris@googlemail.com>
2024-02-21 15:53:43 +0100

    [TASK] maintaince

--- Examples/dummy_system/additional.php
+++ Examples/dummy_system/additional.php
@@ -1,14 +1,5 @@
 <?php
 defined('TYPO3') or die();
 
-/*
-    !IMPORTANT!
-    The file '$databaseCredentialsFile' is not versioned and must be created and changed!
-    For security reasons please create the database accesses outside the document roots!
-
-    Die Datei '$databaseCredentialsFile' wird nicht mit Versioniert und muss extra angelegt und geändert werden!
-    aus Sicherheitsgründen bitte die Datenbank zugänge außerhalb des document roots anlegen!
-*/
-
 $themeDefaultAdditionalConfiguration = \TYPO3\CMS\Core\Core\Environment::getPublicPath() . '/typo3conf/ext/{{EXTENSION_KEY}}/system/additional.php';
 if (file_exists($themeDefaultAdditionalConfiguration)) { require_once ($themeDefaultAdditionalConfiguration); }
