d240e941709cf4db29463cc085df29698233a394
Christian Hackl <hackl.chris@googlemail.com>
2024-02-22 11:17:07 +0100

    [TASK] added ckeditor5 plugin "highlight" as workaround for the inline "font" plugin

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -3,10 +3,14 @@
 return [
     // required import configurations of other extensions,
     // in case a module imports from another package
-    // 'dependencies' => ['backend'],
+    'dependencies' => ['backend'],
+    'tags' => [
+        'backend.form',
+    ],
     'imports' => [
         // recursive definiton, all *.js files in this folder are import-mapped
         // trailing slash is required per importmap-specification
-        '@HauerHeinrich/{{EXTENSION_NAMESPACE_ES6}}/' => 'EXT:{{EXTENSION_KEY}}/Resources/Public/JavaScript/Backend/',
+        '@{{EXTENSION_VENDOR}}/{{EXTENSION_NAMESPACE_ES6}}/' => 'EXT:{{EXTENSION_KEY}}/Resources/Public/JavaScript/Backend/',
+        '@{{EXTENSION_VENDOR}}/ckeditor5-highlight' => 'EXT:{{EXTENSION_KEY}}/Resources/Public/JavaScript/@ckeditor/ckeditor5-highlight.js',
     ],
 ];
