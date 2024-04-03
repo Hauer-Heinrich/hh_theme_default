e974773d88fef800229104c03849035ad2f271f5
Christian Hackl <hackl.chris@googlemail.com>
2024-04-03 11:02:32 +0200

    [FIX] typo for backend javascript namespace modules

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -10,6 +10,6 @@ return [
     'imports' => [
         // recursive definiton, all *.js files in this folder are import-mapped
         // trailing slash is required per importmap-specification
-        '@{{EXTENSION_VENDOR}}/{{EXTENSION_NAMESPACE_ES6}}/' => 'EXT:{{EXTENSION_KEY}}/Resources/Public/JavaScript/Backend/',
+        '@{{EXTENSION_VENDOR}}/{{EXTENSION_NAMESPACE_ES6}}/' => 'EXT:{{EXTENSION_KEY}}/Resources/Public/JavaScript/backend/',
     ],
 ];
