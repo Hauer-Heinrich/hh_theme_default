c56b0f4866f77f7ed9e19937c3be12f160f24e99
tbruemmer <tim_bruemmer@web.de>
2025-04-09 11:06:01 +0200

    refactoring

---
+++ scripts/replace-variables.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
+// Variablen laden
+const config = require('project-config.json');
+
+// Liste aller zu scannenden Dateien
+function getAllFiles(dir, files = []) {
+  fs.readdirSync(dir).forEach(file => {
+    const fullPath = path.join(dir, file);
+
+    if (fs.statSync(fullPath).isDirectory()) {
+      getAllFiles(fullPath, files);
+    } else if (!file.toLowerCase().includes('readme')) {
+      files.push(fullPath);
+    }
+  });
+
+  return files;
+}
+
+// Ersetzungen durchführen
+function replaceVariables(filePath, replacements) {
+  let content = fs.readFileSync(filePath, 'utf8');
+
+  let modified = false;
+  for (const [key, value] of Object.entries(replacements)) {
+    const regex = new RegExp(escapeRegExp(value), 'g');
+    const placeholder = `{{${key}}}`;
+    if (regex.test(content)) {
+      content = content.replace(regex, placeholder);
+      modified = true;
+    }
+  }
+
+  if (modified) {
+    fs.writeFileSync(filePath, content, 'utf8');
+    console.log(`✔ Ersetzt in: ${filePath}`);
+  }
+}
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+// Hauptlogik
+const allFiles = getAllFiles('.', []);
+allFiles.forEach(file => replaceVariables(file, config));
