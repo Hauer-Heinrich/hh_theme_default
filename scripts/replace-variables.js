77a42adb5b5cfaa386ca993a9ef39a1abe4b06ed
tbruemmer <tim_bruemmer@web.de>
2025-04-09 11:34:42 +0200

    test

--- scripts/replace-variables.js
+++ scripts/replace-variables.js
@@ -1,48 +1,52 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // Variablen laden
-const config = require('project-config.json');
+const config = require("./project-config.json");
 
 // Liste aller zu scannenden Dateien
 function getAllFiles(dir, files = []) {
-  fs.readdirSync(dir).forEach(file => {
-    const fullPath = path.join(dir, file);
+    const ignoredDirs = [".git", "node_modules", ".idea"];
 
-    if (fs.statSync(fullPath).isDirectory()) {
-      getAllFiles(fullPath, files);
-    } else if (!file.toLowerCase().includes('readme')) {
-      files.push(fullPath);
-    }
-  });
+    fs.readdirSync(dir).forEach((file) => {
+        const fullPath = path.join(dir, file);
 
-  return files;
+        if (fs.statSync(fullPath).isDirectory()) {
+            if (!ignoredDirs.includes(file)) {
+                getAllFiles(fullPath, files);
+            }
+        } else if (!file.toLowerCase().includes("readme")) {
+            files.push(fullPath);
+        }
+    });
+
+    return files;
 }
 
 // Ersetzungen durchführen
 function replaceVariables(filePath, replacements) {
-  let content = fs.readFileSync(filePath, 'utf8');
+    let content = fs.readFileSync(filePath, "utf8");
 
-  let modified = false;
-  for (const [key, value] of Object.entries(replacements)) {
-    const regex = new RegExp(escapeRegExp(value), 'g');
-    const placeholder = `{{${key}}}`;
-    if (regex.test(content)) {
-      content = content.replace(regex, placeholder);
-      modified = true;
+    let modified = false;
+    for (const [key, value] of Object.entries(replacements)) {
+        const regex = new RegExp(escapeRegExp(value), "g");
+        const placeholder = `{{${key}}}`;
+        if (regex.test(content)) {
+            content = content.replace(regex, placeholder);
+            modified = true;
+        }
     }
-  }
 
-  if (modified) {
-    fs.writeFileSync(filePath, content, 'utf8');
-    console.log(`✔ Ersetzt in: ${filePath}`);
-  }
+    if (modified) {
+        fs.writeFileSync(filePath, content, "utf8");
+        console.log(`✔ Ersetzt in: ${filePath}`);
+    }
 }
 
 function escapeRegExp(string) {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
 
 // Hauptlogik
 const allFiles = getAllFiles(".", []);
-allFiles.forEach(file => replaceVariables(file, config));
+allFiles.forEach((file) => replaceVariables(file, config));
